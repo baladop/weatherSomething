2bdca5dadcef82c325e59262ee13871f7e8497c7
Eben <eben@glucode.com>
2016-09-12 22:32:54 +0200

    Added table view controller

---
+++ weatherApp/CityTableViewController.h
@@ -0,0 +1,13 @@
+//
+//  CityTableViewController.h
+//  weatherApp
+//
+//  Created by Eben Kok on 2016/09/12.
+//  Copyright © 2016 com. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface CityTableViewController : UITableViewController
+
+@end
