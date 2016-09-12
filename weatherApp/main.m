c1f5a82778af94b25c252d60dc7b82c9ef5f0473
Eben <eben@glucode.com>
2016-09-12 22:25:10 +0200

    Initial Commit

---
+++ weatherApp/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  weatherApp
+//
+//  Created by Eben Kok on 2016/09/12.
+//  Copyright © 2016 com. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
