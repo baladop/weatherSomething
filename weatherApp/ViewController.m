029869aa4946f1eb68aee0b37dafbdcd6dba2877
Eben <eben@glucode.com>
2016-09-12 22:53:49 +0200

    Swopped out UIViewController for UITableViewController

--- weatherApp/ViewController.m
+++ weatherApp/ViewController.m
@@ -17,11 +17,21 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
+    self.title = @"Some title";
 }
 
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
+}
+
+- (NSInteger)numberOfSectionsInTableView:(UITableView *)tableView {
+    return 1;
+}
+
+- (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section {
+
+    return 0;
 }
 
 @end
